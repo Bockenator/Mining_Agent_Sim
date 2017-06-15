5c6736c08a6845f1ed81f669bae78f49b5322b00
Bockenator <tom.henry.bock@gmail.com>
2017-06-15 16:33:15 +0100

    Added get_percept method for agent and added mining range checker

--- src/BackEnd/Agent/Agent.java
+++ src/BackEnd/Agent/Agent.java
@@ -11,11 +11,17 @@ public class Agent {
     float x,y,z;
     float x_sp,y_sp,z_sp;
     float sense_range;
+    float mining_range;
     float max_cargo;
     float cargo;
     Action[] action_queue;
+
+    //array for beliefs for logic inference
     ArrayList<String> beliefs;
-    ArrayList<Object> asteroids;
+
+    //these map actual percepts
+    ArrayList<float []> obj_positions;
+    ArrayList<String> obj_names;
 
     public Agent() {
         init();
@@ -30,6 +36,7 @@ public class Agent {
         this.y_sp = 0;
         this.z_sp = 0;
         this.sense_range = 10;
+        this.mining_range = 5;
         this.cargo = 0;
         this.max_cargo = 100;
     }
@@ -41,10 +48,15 @@ public class Agent {
         z+=z_sp;
     }
 
-    //get 2d representation for the front end
-    public int[] get2DAgent(){
-        int[] agent =  {(int)x,(int)y};
-        return agent;
+    //check if any asteroids are in range to be mined
+    public boolean check_range(float [] obj_pos){
+        float distance = (float)Math.sqrt(((obj_pos[0]-x)*(obj_pos[0]-x)) + ((obj_pos[1]-y)*(obj_pos[1]-y)) +
+                ((obj_pos[2]-z)*(obj_pos[2]-z)));
+
+        if (distance <= mining_range){
+            return true;
+        }
+        return false;
     }
 
     //used for doing actions (from action queue)
@@ -66,6 +78,12 @@ public class Agent {
         }
     }
 
+    //percepts come in the form a list of object name (in the future might turn into unique ids) and positions
+    public void get_percepts(ArrayList<String> objects, ArrayList<float []> positions){
+        this.obj_names = objects;
+        this.obj_positions = positions;
+    }
+
     //convert percepts to beliefs#
     public void see(){
         if (cargo > 0) {
@@ -74,6 +92,7 @@ public class Agent {
                 add_belief("cargo_full");
             }
         }
+
 
     }
 
@@ -90,5 +109,11 @@ public class Agent {
         if (beliefs.contains(belief)) {
             beliefs.remove(belief);
         }
+    }
+
+    //get 2d representation for the front end
+    public int[] get2DAgent(){
+        int[] agent =  {(int)x,(int)y};
+        return agent;
     }
 }
